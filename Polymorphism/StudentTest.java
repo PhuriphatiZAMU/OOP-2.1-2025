010dd2c5b373532a5dcb6050c4adeaa7f741bbfa
Phuriphat Hemakul <Noproplayerwork@gmail.com>
2025-08-25 09:22:52 +0700

    try

--- Polymorphism/StudentTest.java
+++ Polymorphism/StudentTest.java
@@ -15,11 +15,10 @@ class Student {
     
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;            
-        if (obj == null) return false;
-        if (!(obj instanceof Student)) return false;
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
         Student other = (Student) obj;
-        return this.id.equals(other.id);         
+        return this.id.equals(other.id);
     }
 
     public class StudentTest {
